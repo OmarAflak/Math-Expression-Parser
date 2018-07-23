f7f3f5cc4764c45b392236ca1e28075a66332068
Omar Aflak <aflakomar@gmail.com>
2018-07-23 11:31:48 +0200

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "../include/processor.h"
 
 int main(int argc, char const *argv[]) {
-    std::string expression = "add(a, pow(b, c))";
+    std::string expression = "a+b^c";
     std::unordered_map<std::string, double> map = {{"a", 1}, {"b", 2}, {"c", 3}};
     std::cout << Processor::evaluate(expression, map) << std::endl;
     return 0;
