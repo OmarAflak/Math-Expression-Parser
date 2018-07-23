13f2ababaab6c396e51a05df83fe1112fbb77ce4
Omar Aflak <aflakomar@gmail.com>
2018-07-23 14:13:01 +0200

    removed modulo

--- include/functions.h
+++ include/functions.h
@@ -15,25 +15,22 @@ static std::string const _SUBTRACT = "subtract";
 static std::string const _MULTIPLY = "multiply";
 static std::string const _DIVIDE = "divide";
 static std::string const _POWER = "pow";
-static std::string const _MODULO = "mod";
 
 static char const _OP_ADD = '+';
 static char const _OP_SUBTRACT = '-';
 static char const _OP_MULTIPLY = '*';
 static char const _OP_DIVIDE = '/';
 static char const _OP_POWER = '^';
-static char const _OP_MODULO = '%';
 
-static std::unordered_set<std::string> const _functions = {_ADD, _SUBTRACT, _MULTIPLY, _DIVIDE, _POWER, _MODULO};
-static std::unordered_set<char> const _op_functions = {_OP_ADD, _OP_SUBTRACT, _OP_MULTIPLY, _OP_DIVIDE, _OP_POWER, _OP_MODULO};
-static std::vector<char> _op_priorities = {_OP_MULTIPLY, _OP_POWER, _OP_DIVIDE, _OP_MODULO};
+static std::unordered_set<std::string> const _functions = {_ADD, _SUBTRACT, _MULTIPLY, _DIVIDE, _POWER};
+static std::unordered_set<char> const _op_functions = {_OP_ADD, _OP_SUBTRACT, _OP_MULTIPLY, _OP_DIVIDE, _OP_POWER};
+static std::vector<char> _op_priorities = {_OP_MULTIPLY, _OP_POWER, _OP_DIVIDE};
 static std::unordered_map<char, std::string> const _op_map_functions = {
     {_OP_ADD, _ADD},
     {_OP_SUBTRACT, _SUBTRACT},
     {_OP_MULTIPLY, _MULTIPLY},
     {_OP_DIVIDE, _DIVIDE},
     {_OP_POWER, _POWER},
-    {_OP_MODULO, _MODULO}
 };
 
 #endif
