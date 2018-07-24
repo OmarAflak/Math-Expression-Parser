7abab47d14f477f60ffa803d93dac3a16b724d48
Omar Aflak <aflakomar@gmail.com>
2018-07-24 09:42:46 +0200

    measure execution time

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <chrono>
 #include "../include/processor.h"
 
+void test(){
+    std::string expression = "exp(x)*y";
+    std::cout << Processor::evaluate(expression, {{"x", 3}, {"y", 2}}) << std::endl;
+}
+
+int measure(void (*function)(void)){
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+    function();
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+}
+
 int main(int argc, char const *argv[]) {
-    std::string expression = "(x+y)^2";
-    std::unordered_map<std::string, double> map = {{"x", 1}, {"y", 2}};
-    std::cout << Processor::evaluate(expression, map) << std::endl;
+    int t = measure(test);
+    std::cout << std::endl << "execution time : " << t << "us" << std::endl;
     return 0;
 }
